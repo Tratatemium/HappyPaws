4c317f6350a2bb4995983fe4b001f41bcfa823e4
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-12-03 19:22:08 +0100

    Refactor header-loader.js to load scripts and styles before fetching HTML for improved performance

--- src/components/Header/header-loader.js
+++ src/components/Header/header-loader.js
@@ -1,16 +1,16 @@
+const script = document.createElement('script');
+script.type = 'module';
+script.src = '/src/components/Header/header.js';    
+document.body.appendChild(script);
+
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = '/src/components/Header/header.css';
+document.head.appendChild(link);
+
 fetch('/src/components/Header/header.html')
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML('afterbegin', html);
-
-    const script = document.createElement('script');
-    script.type = 'module';
-    script.src = '/src/components/Header/header.js';    
-    document.body.appendChild(script);
-
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = '/src/components/Header/header.css';
-    document.head.appendChild(link);
   })
   .catch(err => console.error('Header yüklenemedi:', err));
