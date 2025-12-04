f95139e4f6204763cc87b7f9a5fe73f44adbff39
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-12-04 16:53:32 +0100

    Refactor header-loader.js to load scripts after HTML insertion and remove inline script from header.html

--- src/components/Header/header-loader.js
+++ src/components/Header/header-loader.js
@@ -1,8 +1,3 @@
-const script = document.createElement('script');
-script.type = 'module';
-script.src = '/src/components/Header/header.js';    
-document.body.appendChild(script);
-
 const link = document.createElement('link');
 link.rel = 'stylesheet';
 link.href = '/src/components/Header/header.css';
@@ -12,5 +7,11 @@ fetch('/src/components/Header/header.html')
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML('afterbegin', html);
+    
+    // Load the JavaScript AFTER the HTML is inserted
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = '/src/components/Header/header.js';    
+    document.body.appendChild(script);
   })
   .catch(err => console.error('Header yüklenemedi:', err));
