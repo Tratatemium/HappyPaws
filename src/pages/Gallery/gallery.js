733ae7f17cc9ceb7fb38e22a0a51787367ef80df
eylulbalcilar <eylulbalcilar@gmail.com>
2025-12-05 15:49:27 +0100

    add animal icons and improve gallery UI

--- src/pages/Gallery/gallery.js
+++ src/pages/Gallery/gallery.js
@@ -1,3 +1,64 @@
+// Load pets data
+import pets from '../../assets/pet-data.json';
+
+
+// Select containers
+const urgentContainer = document.getElementById('urgentPetsContainer');
+const availableContainer = document.getElementById('availablePetsContainer');
+
+// Create card element
+function createPetCard(pet) {
+  const isUrgent = pet.urgent === true;
+
+  return `
+    <article class="pet-card ${isUrgent ? 'urgent' : ''}">
+      <div class="pet-image">
+        <img src="${pet.image_urls[0]}" alt="${pet.name}">
+        ${isUrgent ? `<span class="urgent-badge">Urgent</span>` : ''}
+        <div class="paw-badge">
+          <img src="../../assets/icons/paw-like-icon.png">
+        </div>
+        <div class="image-gradient"></div>
+        <div class="info info--overlay">
+          <h3>${pet.name}</h3>
+          <p>${pet.breed} · ${pet.age}</p>
+        </div>
+      </div>
+    </article>
+  `;
+}
+
+// Render pets by urgency
+function renderPets() {
+  pets.forEach(pet => {
+    const cardHTML = createPetCard(pet);
+    if (pet.urgent === true) {
+      urgentContainer.innerHTML += cardHTML;
+    } else {
+      availableContainer.innerHTML += cardHTML;
+    }
+  });
+
+  attachLikeEvents();
+}
+
+// Laura: toggle paw like icon on click for both urgent paw badges and fav buttons
+function attachLikeEvents() {
+  const favButtons = document.querySelectorAll(".paw-badge, .fav-btn");
+
+  favButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const img = btn.querySelector("img");
+
+      const isLiked = btn.classList.toggle("is-liked");
+
+      img.src = isLiked
+      ? "../../assets/icons/paw-liked-red-icon.png"
+      : "../../assets/icons/paw-like-icon.png";
+    });
+  });
+}
+
 // Category button active state
 const chips = document.querySelectorAll(".chip");
 
@@ -8,17 +69,5 @@ chips.forEach((chip) => {
   });
 });
 
-// Laura: toggle paw like icon on click for both urgent paw badges and fav buttons
-const favButtons = document.querySelectorAll(".paw-badge, .fav-btn");
-
-favButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const img = btn.querySelector("img");
-
-    const isLiked = btn.classList.toggle("is-liked");
-
-    img.src = isLiked
-      ? "../../assets/icons/paw-liked-red-icon.png"
-      : "../../assets/icons/paw-like-icon.png";
-  });
-});
+// Run load
+renderPets();
