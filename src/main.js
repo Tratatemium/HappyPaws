595173b688b96b9bd84020426918cd1a40032560
Laura Lundin <laurama1007@gmail.com>
2025-12-08 13:46:19 +0100

    redirect loading page to landing page (#26)
    * redirect loading page to landing page
    
    * redirect index.html to loading page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import { loadPetDataFromJSON } from "./load-data.js"
+import { loadPetDataFromJSON } from "./load-data.js";
 
 export const state = {
-    data: [],
-}
+  data: [],
+};
 
 const init = async () => {
-    await loadPetDataFromJSON();
-    console.log(state);            // Just for development conviniance
+  await loadPetDataFromJSON();
+  console.log(state); // Just for development conviniance
 };
 
 init();
+
+window.addEventListener("DOMContentLoaded", () => {
+  window.location.replace("src/pages/loading-page/loading-page.html");
+});
