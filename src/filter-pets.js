0c9775c9e8fd4f5be578d3233cbdf9fdb7ec8941
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-12-09 15:35:32 +0100

    Rename filterByPetCategory to filterByPetSpecies and update logic for filtering pet cards

--- src/filter-pets.js
+++ src/filter-pets.js
@@ -1,11 +1,18 @@
-export const filterByPetCategory = (category) => {
-    if (!['all','dog', 'cat', 'bird', 'rabbit'].includes(category)) {
-        console.error('Invalid input to filterByPetCategory()');
+export const filterByPetSpecies = (species) => {
+    if (!['all','dog', 'cat', 'bird', 'rabbit'].includes(species)) {
+        console.error('Invalid input to filterByPetSpecies()');
         return;
     }
 
     const petCards = Array.from(document.querySelectorAll('.pet-card'));
-    
+
+    petCards.forEach(petCard => {
+        if (species === 'all' || petCards.classList.contains(species)) {
+            petCards.classList.remove('hidden');
+        } else {
+            petCards.classList.add('hidden');
+        }
+    });
 }   
 
 export const onPetTypeButtonClick = (event) => {
