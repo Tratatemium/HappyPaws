eeb8275d810269204b638a7f6b17cb6d885c240b
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-12-09 15:23:59 +0100

    Refactor filterByType function to filter pets by category and validate input

--- src/filter-pets.js
+++ src/filter-pets.js
@@ -1,3 +1,13 @@
-export const filterByType = (event) => {
+export const filterByPetCategory = (category) => {
+    if (!['all','dog', 'cat', 'bird', 'rabbit'].includes(category)) {
+        console.error('Invalid input to filterByPetCategory()');
+        return;
+    }
+
+    const petCards = Array.from(document.querySelectorAll('.pet-card'));
+    
+}   
+
+export const onPetTypeButtonClick = (event) => {
     button = event.target;
 }
