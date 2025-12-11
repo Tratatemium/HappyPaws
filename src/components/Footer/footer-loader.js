8a912d5e10c9d6a9c4366c952cc2c864f54922cb
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-12-11 13:31:01 +0100

    fix: refactor footer-loader to use module imports and clean up commented code

--- src/components/Footer/footer-loader.js
+++ src/components/Footer/footer-loader.js
@@ -1,10 +1,15 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = '../../components/Footer/footer.css';
-document.head.appendChild(link);
+// const link = document.createElement('link');
+// link.rel = 'stylesheet';
+// link.href = '../../components/Footer/footer.css';
+// document.head.appendChild(link);
 
-fetch('../../components/Footer/footer.html')
-    .then(response => response.text())
-    .then(html => {
-        document.querySelector("footer").innerHTML = html;
-    });
+// fetch('../../components/Footer/footer.html')
+//     .then(response => response.text())
+//     .then(html => {
+//         document.querySelector("footer").innerHTML = html;
+//     });
+
+import footerHtml from './footer.html?raw';
+import './footer.css';
+
+document.querySelector("footer").innerHTML = footerHtml;
