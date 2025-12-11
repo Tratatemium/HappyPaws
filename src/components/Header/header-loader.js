bfb222bae88a9af13e3f30ed719d0fb5dbee7c47
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-12-11 13:18:24 +0100

    fix: refactor header component to use module imports and clean up commented code

--- src/components/Header/header-loader.js
+++ src/components/Header/header-loader.js
@@ -1,17 +1,31 @@
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = '../../components/Header/header.css';
-document.head.appendChild(link);
+// const link = document.createElement('link');
+// link.rel = 'stylesheet';
+// link.href = '../../components/Header/header.css';
+// document.head.appendChild(link);
 
-fetch('../../components/Header/header.html')
-  .then(res => res.text())
-  .then(html => {
-    document.body.insertAdjacentHTML('afterbegin', html);
+// fetch('../../components/Header/header.html')
+//   .then(res => res.text())
+//   .then(html => {
+//     document.body.insertAdjacentHTML('afterbegin', html);
     
-    // Load the JavaScript AFTER the HTML is inserted
-    const script = document.createElement('script');
-    script.type = 'module';
-    script.src = '../../components/Header/header.js';    
-    document.body.appendChild(script);
-  })
-  .catch(err => console.error('Header yüklenemedi:', err));
+//     // Load the JavaScript AFTER the HTML is inserted
+//     const script = document.createElement('script');
+//     script.type = 'module';
+//     script.src = '../../components/Header/header.js';    
+//     document.body.appendChild(script);
+//   })
+//   .catch(err => console.error('Header yüklenemedi:', err));
+
+
+import headerHtml from './header.html?raw';
+import './header.css';
+import initHeader from './header.js';
+
+document.body.insertAdjacentHTML('afterbegin', headerHtml);
+
+// ---- Run the header JS after the HTML is added ----
+if (typeof initHeader === 'function') {
+  initHeader();
+} else {
+  console.warn("header.js did not export a default function.");
+}
