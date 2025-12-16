bf76e66d57c96edd92693224b57d96cf7f008f3a
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2025-12-16 11:25:23 +0100

    Update checkout page to include pet name in adoption reason and improve code structure

--- src/pages/Checkout/checkout.js
+++ src/pages/Checkout/checkout.js
@@ -1,43 +1,24 @@
-
-
 import pets from "../../assets/pet-data.json";
-const termsToggle = document.getElementById('toggle');
- const submitBtn = document.getElementById('submit-adoption');
- const adoptionForm = document.querySelector('form');
-       const requiredInputs = document.querySelectorAll('.input');
+const termsToggle = document.getElementById("toggle");
+const submitBtn = document.getElementById("submit-adoption");
+const adoptionForm = document.querySelector("form");
+const requiredInputs = document.querySelectorAll(".input");
 
+function getPetIdFromUrl() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("petId");
+}
 
+function gtePetById(petId) {
+  return pets.find((pet) => pet.id === petId);
+}
 
+function renderCheckoutPetCard(pet) {
+  const petcard = document.querySelector(".pet-card");
 
+  if (!pet) return;
 
-
-       function getPetIdFromUrl(){
-         const params = new URLSearchParams(window.location.search);
-         return params.get('petId');
-       }
-    
-
-
-       function gtePetById(petId){
-return pets.find(pet => pet.id === petId);
-       }
-
-
-
-
-       function renderCheckoutPetCard(pet){
-     
-         const petcard = document.querySelector(".pet-card");
-
-
-
-
-         if(!pet)return;
-
-
-
-
-        petcard.innerHTML = `
+  petcard.innerHTML = `
    <img src="${pet.image_urls[0]}" alt="${pet.name}" class="checkout-pet-img" />
 
 
@@ -49,20 +30,16 @@ return pets.find(pet => pet.id === petId);
    </div>
  `;
 
+  const labelPetName = document.getElementById("label-pet-name");
+  labelPetName.textContent = pet.name;
+}
 
-       }
-
-
-
-
-document.addEventListener('DOMContentLoaded', ()=>{
-
-
-       const list = document.createElement('div');
-       list.classList.add('generated-terms')
-       list.classList.add('hidden');
-       const listContainer = document.querySelector('.term-list')
-       list.innerHTML=`     <div class='ul-list'>
+document.addEventListener("DOMContentLoaded", () => {
+  const list = document.createElement("div");
+  list.classList.add("generated-terms");
+  list.classList.add("hidden");
+  const listContainer = document.querySelector(".term-list");
+  list.innerHTML = `     <div class='ul-list'>
        <p> By adopting a pet from HappyPaws, you agree to:</p>
        <ul>
 <li>
@@ -85,127 +62,88 @@ Keep the pet indoors and provide a safe environment </li>
    <p>I accept the terms and conditions of adoption and understand the responsibilities involved in pet ownership </p>
       </div>
         
-       `   
+       `;
 
+  termsToggle.addEventListener("click", () => {
+    termsToggle.classList.toggle("active");
 
+    const currentTerms = listContainer.querySelector(".generated-terms");
 
+    if (currentTerms) {
+      currentTerms.classList.toggle("hidden");
+    } else {
+      listContainer.innerHTML = list;
+      termsToggle.classList.toggle("active");
+    }
+  });
 
-       termsToggle.addEventListener('click', ()=>{
-         termsToggle.classList.toggle('active');
-          
-           const currentTerms = listContainer.querySelector('.generated-terms');
-        
-           if(currentTerms){
-           currentTerms.classList.toggle('hidden');
-     
-           }else{
-             listContainer.innerHTML = list;
-                termsToggle.classList.toggle('active');
-               
-           }
-       })
+  const petId = getPetIdFromUrl();
 
+  if (!petId) {
+    console.error("No pet ID in URL");
+    return;
+  }
 
+  const pet = gtePetById(petId);
 
+  if (!pet) {
+    console.error("Pet not found");
+    return;
+  }
 
+  renderCheckoutPetCard(pet);
 
+  listContainer.appendChild(list);
+});
 
-       const petId = getPetIdFromUrl()
-
-
- if (!petId) {
-   console.error("No pet ID in URL");
-   return;
- }
-
-
- const pet = gtePetById(petId)
-
-
- if (!pet) {
-   console.error("Pet not found");
-   return;
- }
-
-
-renderCheckoutPetCard(pet);
-
-
-       
-listContainer.appendChild(list)
-      });
-
-
-
-
-      
 // I have added form validation to ensure all required fields are filled before submission and also preventing default submit action.
 
+submitBtn.addEventListener("click", (event) => {
+  let formIsvalid = true;
 
-      submitBtn.addEventListener('click', (event)=>{
+  document.querySelectorAll(".error-text").forEach((error) => {
+    error.textContent = "";
+  });
 
+  requiredInputs.forEach((input) => {
+    const errorContainer = document.getElementById(input.id + "-error");
 
-       let formIsvalid =true;
-    
- document.querySelectorAll('.error-text').forEach(error=>{
-   error.textContent= '';
- })
+    if (input.value.trim() === "") {
+      event.preventDefault();
+      formIsvalid = false;
+      const errorText = document.createElement("p");
+      errorText.classList.add("error-text");
+      errorText.textContent = `PLease fill in your ${input.id.toUpperCase()} `;
+      errorText.style.color = "red";
+      if (errorContainer) {
+        errorContainer.appendChild(errorText);
+      }
+    }
 
-
- requiredInputs.forEach(input =>{
-  
-   const errorContainer = document.getElementById(input.id + '-error');
-
-
-  
-
-
-       if(input.value.trim() === ''){
-       event.preventDefault();
-   formIsvalid = false;
-           const errorText = document.createElement('p');
-       errorText.classList.add('error-text');
-       errorText.textContent= `PLease fill in your ${input.id.toUpperCase()} `;
-       errorText.style.color ='red';
-       if(errorContainer){
- errorContainer.appendChild(errorText);
-       }
-
-
-       }
-
-
-       if(formIsvalid){
-         console.log('valid')
-         event.preventDefault();
-         showToast('Application Submitted Successfully!');
-       }
- })
-      })
-
-
-
-
-
+    if (formIsvalid) {
+      console.log("valid");
+      event.preventDefault();
+      showToast("Application Submitted Successfully!");
+    }
+  });
+});
 
 // This is a toast message that will appear for 3 seconds when the form is sucessfully submitted
 function showToast(message) {
-  
-   const toast = document.getElementById('toast');
-   const toastText =document.createElement('p');
+  const toast = document.getElementById("toast");
+  const toastText = document.createElement("p");
 
+  if (toast) {
+    if (message) {
+      toast.textContent = message;
+    }
 
-   if (toast) {
-       if (message) {
-           toast.textContent = message;
-       }
-      
-       toast.classList.add('show');
-         toast.style.display ='block';
-               toast.appendChild(toastText);
-       setTimeout(() => {
-                 toast.classList.remove('show');
-           toast.style.display ='none'
-       }, 3000);
-   }
+    toast.classList.add("show");
+    toast.style.display = "block";
+    toast.appendChild(toastText);
+    setTimeout(() => {
+      toast.classList.remove("show");
+      toast.style.display = "none";
+    }, 3000);
+  }
 }
